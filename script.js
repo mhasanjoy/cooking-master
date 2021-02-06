40b243b87141b3e21ab3069e7f302465a78fe953
Md. Mehedi Hasan <m.hasanjoy13@gmail.com>
2021-02-07 04:37:29 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const ingredientsSection = () => {
+    const ingredients = document.getElementById("ingredients");
+    ingredients.innerHTML = null;
+    return ingredients;
+};
+
+
+const displayIngredients = (meal, mealDiv) => {
+    mealDiv.addEventListener("click", function(){
+        const ingredients = ingredientsSection();
+        const ingredientsDiv = document.createElement("div");
+        ingredientsDiv.innerHTML = `
+                <img src="${meal.strMealThumb}">
+                <h2>${meal.strMeal}</h2>
+                <p>Ingredients</p>
+                <ul>
+                    <li>${meal.strIngredient1}</li>
+                    <li>${meal.strIngredient2}</li>
+                    <li>${meal.strIngredient3}</li>
+                    <li>${meal.strIngredient4}</li>
+                    <li>${meal.strIngredient5}</li>
+                </ul>
+            `;
+            ingredients.appendChild(ingredientsDiv);
+    });
+};
+
+
+const displayMeals = meals => {
+    const mealsSection = document.getElementById("meals");
+    mealsSection.innerHTML = null;
+    const ingredients = ingredientsSection();
+    meals.forEach(meal => {
+        const mealDiv = document.createElement("div");
+        mealDiv.className = "meal";
+        mealDiv.innerHTML = `
+            <img src="${meal.strMealThumb}">
+            <p>${meal.strMeal}</p>
+        `;
+        mealsSection.appendChild(mealDiv);
+
+        displayIngredients(meal, mealDiv);
+    });
+};
+
+
+document.getElementById("search-btn").addEventListener("click", function(){
+    const searchingText = document.getElementById("searching-text").value;
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchingText}`)
+    .then(response => response.json())
+    .then(data => displayMeals(data.meals))
+    .catch(error => {
+        const ingredients = ingredientsSection();
+        ingredients.innerHTML = `
+            <h1 id="error">Your search - ${searchingText} - did not match any food items.</h1>
+        `;
+    });
+});
